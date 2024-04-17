89ca69a09d93223bbe34c5c866f0b20a41968bf1
Clinton.G <clintongoin@gmail.com>
2024-04-16 20:29:44 -0500

    week3day1

---
+++ 1_calculator.py
@@ -0,0 +1,56 @@
+#   Task 1:
+
+
+# lie1 = "Taco's"
+# lie2 = 'Are'
+# lie3 = 'Goatted'
+# lies = lie1 + ' ' + lie2 + ' ' + lie3
+# print(lies)
+
+#   Add:
+fact1 = "Taco's"
+fact2 = "Are"
+fact3 = "Mid"
+truth = fact1 + ' ' + fact2 + ' ' + fact3
+print(truth)
+
+#   Subtract:
+subtract = 100 - 50 - 25
+print(subtract)
+
+#   Multiply: 
+multiply = 10*10
+print(multiply)
+
+#   Divide:
+divide = 100 / 2
+print(divide)
+
+
+
+#----------------------------------------------------------------------------------------------------
+#   Task 2:
+# Implement user input to receive numbers and an operation choice.
+
+print("1. Addition ")
+print("2. Subtract ")
+print("3. Multiply ")
+print("4. Divide ")
+print(input('Pick a Math Format (1, 2, 3, 4): '))
+
+
+print(input('enter first number: '))
+print(input('enter second number: '))
+
+
+
+
+#----------------------------------------------------------------------------------------------------
+#   Task 3:
+
+print('I Honestly Dont Remember How')
+
+
+
+
+
