0c363b912d231ae653fdfb89edffa9cae84e879b
Clinton-G <166076564+Clinton-G@users.noreply.github.com>
2024-05-29 19:06:43 -0500

    Add files via upload

--- 1_calculator.py
+++ 1_calculator.py
@@ -1,56 +1,41 @@
-#   Task 1:
+def add(num1, num2):
+    return num1 + num2
 
+def subtract(num1, num2):
+    return num1 - num2
 
-# lie1 = "Taco's"
-# lie2 = 'Are'
-# lie3 = 'Goatted'
-# lies = lie1 + ' ' + lie2 + ' ' + lie3
-# print(lies)
+def multiply(num1, num2):
+    return num1 * num2
 
-#   Add:
-fact1 = "Taco's"
-fact2 = "Are"
-fact3 = "Mid"
-truth = fact1 + ' ' + fact2 + ' ' + fact3
-print(truth)
+def divide(num1, num2):
+    if num2 == 0:
+        return "Invalid Imput, Try Again"
+    else:
+        return num1 / num2
 
-#   Subtract:
-subtract = 100 - 50 - 25
-print(subtract)
+def calculator():
+    while True:
+        #had to get this input part from the internet cause i couldnt find any way to do it
+        num1, num2, type = input("Enter num1, num2, then the type seperated by spaces (+, -, *, /): ").split()
 
-#   Multiply: 
-multiply = 10*10
-print(multiply)
+        num1 = float(num1)
+        num2 = float(num2)
 
-#   Divide:
-divide = 100 / 2
-print(divide)
+        if type == '+':
+            print("Result:", add(num1, num2))
+        elif type == '-':
+            print("Result:", subtract(num1, num2))
+        elif type == '*':
+            print("Result:", multiply(num1, num2))
+        elif type == '/':
+            print("Result:", divide(num1, num2))
+        else:
+            print("Invalid Input, Try Again")
 
+        choice = input("Go Again? (yes/no): ")
+        if choice.lower() != 'yes':
+            break
 
-
-#----------------------------------------------------------------------------------------------------
-#   Task 2:
-# Implement user input to receive numbers and an operation choice.
-
-print("1. Addition ")
-print("2. Subtract ")
-print("3. Multiply ")
-print("4. Divide ")
-print(input('Pick a Math Format (1, 2, 3, 4): '))
-
-
-print(input('enter first number: '))
-print(input('enter second number: '))
-
-
-
-
-#----------------------------------------------------------------------------------------------------
-#   Task 3:
-
-print('I Honestly Dont Remember How')
-
-
-
-
-
+        # if choice == "no":
+        #     print("Have a Good Day")
+calculator()
--- 3_grade.py
+++ 3_grade.py
@@ -1,20 +1,7 @@
-#given a list of grades, this function will calc. avg. grade
-#add a function that finds the highest, and the lowest grade in the list
-#create a list that grades an assignment into a, b, c, d, f. add funtciton to sort the grade list in order from a-f
-
-#Task 1: Code a function to calculate the average grade. 
-#Task 2: Implement a function to find the highest and lowest grade. 
-#Task 3 (BONUS): Create a feature that categorizes grades into letter grades (A, B, C, etc.).
-
-
-
-#   Task 1:
-
 grades = [95, 55, 10, 65, 75, 85]
 average = sum(grades) / 6
 print('The Average Grade is:', round(average))
 # print(round(average))
-
 
 
 #   Task 2:
@@ -25,8 +12,6 @@ print('The Highest Grade is:', max_grade)
 
 min_grade = min(grades)
 print('The Lowest Grade is:', min_grade)
-
-
 
 
 #   Task 3:
@@ -44,7 +29,4 @@ for grade in grades:
         pass
     else:
         pass
-
-
-
 
